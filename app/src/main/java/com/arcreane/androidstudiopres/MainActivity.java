44f005a85062c9969136728aeb3509b0f66be6a6
Dominique YOLIN <d.yolin@isartdigital.com>
2025-02-21 11:59:36 +0100

    End of exercise 1

--- app/src/main/java/com/arcreane/androidstudiopres/MainActivity.java
+++ app/src/main/java/com/arcreane/androidstudiopres/MainActivity.java
@@ -10,10 +10,18 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-     setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
 //        TextView tv = new TextView(this);
 //        tv.setText("Hello through Java only");
 //        tv.setTextSize(60);
 //        setContentView(tv);
+
+        TextView tv4 = findViewById(R.id.textView4);
+        TextView tv5 = findViewById(R.id.textView5);
+
+        tv4.setText(R.string.changed_with_r_string);
+
+        String textToChange = getResources().getString(R.string.changed_with_getressources);
+        tv5.setText(textToChange);
     }
 }
