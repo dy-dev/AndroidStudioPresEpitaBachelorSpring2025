0068b35b08ff58b509dada6a6f62ae07aa5e7c13
Dominique YOLIN <d.yolin@isartdigital.com>
2025-02-21 14:47:20 +0100

    End of exercise 3

--- app/src/main/java/com/arcreane/androidstudiopres/Exercise2.java
+++ app/src/main/java/com/arcreane/androidstudiopres/Exercise2.java
@@ -3,11 +3,7 @@ package com.arcreane.androidstudiopres;
 import android.os.Bundle;
 import android.widget.RelativeLayout;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class Exercise2 extends AppCompatActivity {
 
@@ -17,6 +13,6 @@ public class Exercise2 extends AppCompatActivity {
         setContentView(R.layout.activity_exercise2);
 
         RelativeLayout main = findViewById(R.id.main);
-        main.setBackgroundColor(getResources().getColor(R.color.greendish, getTheme()));
+        main.setBackgroundColor(getResources().getColor(R.color.greenish, getTheme()));
     }
 }
---
+++ app/src/main/java/com/arcreane/androidstudiopres/GuessANumber.java
@@ -0,0 +1,19 @@
+package com.arcreane.androidstudiopres;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class GuessANumber extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guess_a_number);
+
+    }
+}
